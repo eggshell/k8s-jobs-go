42f2dffec07e2b9669e2d7ff46fd21e26609389a
Cullen Taylor <mctaylor@us.ibm.com>
2018-07-17 15:33:20 -0500

    Running but not successfully creating job
    Signed-off-by: Cullen Taylor <mctaylor@us.ibm.com>

---
+++ list_jobinfo.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "k8s.io/client-go/util/homedir"
+    "path/filepath"
+    /* oidc needed for auth to IBM Cloud but is not referenced specifically in
+    this script */
+    _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+    v1 "k8s.io/api/core/v1"
+    batchv1 "k8s.io/api/batch/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/client-go/kubernetes"
+    "k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+    var kubeconfig *string
+    if home := homedir.HomeDir(); home != "" {
+        kubeconfig = flag.String("kubeconfig", filepath.Join(home, "kubeconfig.yml"), "(optional) absolute path to the kubeconfig file")
+    } else {
+        kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+    }
+    flag.Parse()
+
+    config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+    if err != nil {
+        panic(err)
+    }
+    clientset, err := kubernetes.NewForConfig(config)
+    if err != nil {
+        panic(err)
+    }
+
+    jobsClient := clientset.BatchV1().Jobs("default")
+    job := &batchv1.Job{
+        ObjectMeta: metav1.ObjectMeta{
+            Name: "whalesay-job",
+            Namespace: "default",
+        },
+        Spec: batchv1.JobSpec{
+            Template: v1.PodTemplateSpec{
+                Spec: v1.PodSpec{
+                    Containers: []v1.Container{
+                        {
+                            Name:  "whalesay",
+                            Image: "docker/whalesay",
+                        },
+                    },
+                },
+            },
+        },
+    }
+
+    fmt.Println("Creating job... ")
+    result1, err1 := jobsClient.Create(job)
+    if err != nil {
+        fmt.Println(err1)
+        panic(err1)
+    }
+    fmt.Printf("Created job %q.\n", result1)
+
+    fmt.Println("Listing jobs....")
+    fmt.Println(jobsClient.List(metav1.ListOptions{}))
+}
