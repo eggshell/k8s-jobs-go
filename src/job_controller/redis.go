f543e82f1ccce49c0d1005b9597464c6b6095ac7
Cullen Taylor <mctaylor@us.ibm.com>
2018-08-02 14:43:14 -0500

    Add redis env vars to job-controller.yaml
    Signed-off-by: Cullen Taylor <mctaylor@us.ibm.com>

--- src/job_controller/redis.go
+++ src/job_controller/redis.go
@@ -8,7 +8,7 @@ import (
 
 func GetRedisClient() redis.Client {
     client := redis.NewClient(&redis.Options{
-        Addr:       os.Getenv("REDIS_HOST") + ":" + os.Getenv("REDIS_PORT"),
+        Addr:       "redis-master:6379",
         Password:   os.Getenv("REDIS_PASSWORD"),
         DB:         0,
     })
